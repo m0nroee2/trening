ee72f0fbb631313d132e83d4f10c1e4770eb44c1
Арсений Бочарников <senboch@gmail.com>
2022-11-09 20:07:59 +0100

    Добавьте файлы проекта.

---
+++ trening/trening.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,m;
+	
+	cout << "How much yor acamulator? " << endl;
+	cin >> n;
+	
+	m = n / 500;
+	cout << m << endl;
+	
+	if (m > 6) 
+		cout << "twoi akum zaebis" << endl;
+	else
+		cout << "twoi akum kak u denisa-tupoe govnishe" << endl;
+}
